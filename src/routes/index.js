bc6737ed94cd8ccc0896547f4508d107e520514a
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-12-13 21:11:10 +0100

    FLEET-17 CRUD Remorques

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import authRoutes from './auth.js';
 import truckRoutes from './truck.routes.js';
+import trailerRoutes from './trailer.routes.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get('/', (req, res) => {
 });
 router.use('/auth', authRoutes);
 router.use('/trucks', truckRoutes);
+router.use('/trailers', trailerRoutes);
 
 export default router;
